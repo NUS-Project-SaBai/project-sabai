c0dab62c08ef4ec858c577f1af164fb70bacca85
Keiran <keinn2210@gmail.com>
2026-02-25 19:25:31 +0800

    Add medication db seeds

---
+++ supabase/seeds/03_medication_brands.sql
@@ -0,0 +1,11 @@
+INSERT INTO medication_brands (
+    id, 
+    name, 
+    active_ingredient_id
+) VALUES
+(1, 'Panadol', 1),
+(2, 'Tylenol', 1),
+(3, 'Amoxil', 2),
+(4, 'Brufen', 3),
+(5, 'Glucophage', 4),
+(6, 'Lipitor', 5);
