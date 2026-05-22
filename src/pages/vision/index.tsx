272dcb570c1b068dac962f409b84a393d1b07946
Derrick Low <lowderrickym@gmail.com>
2026-05-22 23:16:05 +0800

    feat: update index.tsx for vision to use TableRow and TableCell components

--- src/pages/vision/index.tsx
+++ src/pages/vision/index.tsx
@@ -6,6 +6,8 @@ import { PatientPhoto } from "@/components/PatientPhoto";
 import { HiOutlinePencilSquare } from "react-icons/hi2";
 import LoadingSpinner from "@/components/LoadingSpinner";
 import TableHeader from "@/components/TableHeader";
+import TableRow from "@/components/TableRow";
+import TableCell from "@/components/TableCell";
 import { Button } from "@/components/interactive/Button/Button";
 
 function VisionPage() {
@@ -43,39 +45,34 @@ function VisionPage() {
                 />
                 <tbody className="bg-white divide-y divide-slate-200">
                   {patients.map((patient) => (
-                    <tr
-                      key={patient.id}
-                      className="hover:bg-slate-50 transition-colors"
-                    >
-                      <td className="px-6 py-4 whitespace-nowrap">
+                    <TableRow key={patient.id}>
+                      <TableCell>
                         <div className="text-sm font-medium text-slate-900">
                           {patient.id.toString().padStart(4, "0")}
                         </div>
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap">
+                      </TableCell>
+                      <TableCell>
                         <PatientPhoto
                           pictureUrl={patient.patientImageUrl}
                           className="rounded-full border border-slate-200"
                         />
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap">
+                      </TableCell>
+                      <TableCell>
                         <div className="text-sm font-medium text-slate-900">
                           {patient.name}
                         </div>
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap">
+                      </TableCell>
+                      <TableCell>
                         <Button
-                          onClick={() =>
-                            handleUpdateGlasses(patient.id)
-                          }
+                          onClick={() => handleUpdateGlasses(patient.id)}
                           title="Update Glasses"
                           icon={<HiOutlinePencilSquare className="h-4 w-4" />}
                           colour="emerald"
                           variant="filled"
                           size="medium"
                         />
-                      </td>
-                    </tr>
+                      </TableCell>
+                    </TableRow>
                   ))}
                 </tbody>
               </table>
