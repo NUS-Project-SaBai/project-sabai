cf0c93cb2bc92ede625324e1c171e1ef074cd109
Keiran <80461252+keiran0@users.noreply.github.com>
2026-05-24 23:50:50 +0800

    Delete src/components/ErrorScreen.tsx
    Accidentally included draft in this commit

--- src/components/ErrorScreen.tsx
+++
@@ -1,7 +0,0 @@
-export default function ErrorScreen({ error, reset }: { error: Error; reset: () => void }) {
-    return <div>
-        <h1>Oops, something has gone wrong.</h1>
-        <p>{error?.message}</p>
-        <button onClick={reset}>Try again</button>
-    </div>    
-}
