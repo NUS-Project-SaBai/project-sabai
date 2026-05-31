586180cf0f990fd710b5ada92890135526700d66
Derrick Low <lowderrickym@gmail.com>
2026-05-31 20:33:44 +0800

    feat: mock HTMLDialogElement methods to fix jsdom support

--- src/test-setup.ts
+++ src/test-setup.ts
@@ -1,1 +1,15 @@
-import '@testing-library/jest-dom'
+import "@testing-library/jest-dom";
+
+// Workaround for vitest - mock HTMLDialogElement methods
+// https://github.com/jsdom/jsdom/issues/3294
+beforeAll(() => {
+  HTMLDialogElement.prototype.show = vi.fn(function (this: HTMLDialogElement) {
+    this.setAttribute("open", "");
+  });
+  HTMLDialogElement.prototype.showModal = vi.fn(function (this: HTMLDialogElement) {
+    this.setAttribute("open", "");
+  });
+  HTMLDialogElement.prototype.close = vi.fn(function (this: HTMLDialogElement) {
+    this.removeAttribute("open");
+  });
+});
