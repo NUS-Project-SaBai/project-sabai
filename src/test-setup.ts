3c821479bb48248aba0eecd5d0bb327dd1fba3ca
Keiran <keinn2210@gmail.com>
2026-06-08 11:53:18 +0800

    Add matchMedia definition

--- src/test-setup.ts
+++ src/test-setup.ts
@@ -14,4 +14,20 @@ beforeAll(() => {
   HTMLDialogElement.prototype.close = vi.fn(function (this: HTMLDialogElement) {
     this.removeAttribute("open");
   });
+
+  // Jsdom does not implement window.matchMedia by default, see below website for explanation:
+  // https://rebeccamdeprey.com/blog/mock-windowmatchmedia-in-vitest
+  Object.defineProperty(window, "matchMedia", {
+    writable: true,
+    value: vi.fn().mockImplementation((query) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: vi.fn(), // deprecated
+      removeListener: vi.fn(), // deprecated
+      addEventListener: vi.fn(),
+      removeEventListener: vi.fn(),
+      dispatchEvent: vi.fn(),
+    })),
+  });
 });
