0d53891542d07355457df0f3a6ea0c11e4c77fdf
Keiran <keinn2210@gmail.com>
2026-06-16 00:23:11 +0800

    Remove typo

--- src/components/scanFace/MatchingPatients.tsx
+++ src/components/scanFace/MatchingPatients.tsx
@@ -63,7 +63,7 @@ export default function MatchingPatients({
   if (matchingPatients.length === 0) {
     return (
       <>
-        <h2 className="text-xl font-bold mb-4">No matches found</h2>;
+        <h2 className="text-xl font-bold mb-4">No matches found</h2>
         <button
           onClick={() => setMode(Mode.REGISTERING)}
           className="flex-1 px-4 py-2 rounded-lg font-medium bg-red-300"
