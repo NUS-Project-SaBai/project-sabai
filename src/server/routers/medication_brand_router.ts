6a9f0b876d7a4965e3887f6fe459de53ecfb95c8
Keiran <keirankam@gmail.com>
2026-07-02 16:09:54 +0800

    feat: add remarks to medication brand router

--- src/server/routers/medication_brand_router.ts
+++ src/server/routers/medication_brand_router.ts
@@ -12,6 +12,7 @@ export const medicationBrandRouter = router({
         id: medicationBrands.id,
         name: medicationBrands.name,
         activeIngredientId: medicationBrands.activeIngredientId,
+        remarks: medicationBrands.remarks,
       })
       .from(medicationBrands);
     return result;
@@ -22,6 +23,7 @@ export const medicationBrandRouter = router({
       zfd.formData({
         name: zfd.text(z.string().nonoptional()),
         activeIngredientId: zfd.numeric(z.number().int()),
+        remarks: zfd.text().nonoptional(),
       }),
     )
     .mutation(async ({ input }) => {
@@ -38,6 +40,7 @@ export const medicationBrandRouter = router({
         id: zfd.numeric(z.number().int()).nonoptional(),
         name: zfd.text().optional(),
         activeIngredientId: zfd.numeric(z.number().int()),
+        remarks: zfd.text(),
       }),
     )
     .mutation(async ({ input }) => {
