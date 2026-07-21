9a9b3baa43478143930d60acb1c4b56614a3a529
Keiran <keirankam@gmail.com>
2026-07-21 10:16:27 +0800

    fix: fix date formatting in test

--- src/__tests__/pages/medication-stock/index.test.tsx
+++ src/__tests__/pages/medication-stock/index.test.tsx
@@ -140,7 +140,7 @@ describe("MedicationStockPage", () => {
         "Panadol",
         "Shelf 1",
         "200",
-        "5/21/2026",
+        "21/05/2026",
         "active",
         "EditSplit",
       ],
