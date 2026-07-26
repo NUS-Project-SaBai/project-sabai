6224c396cf19001d58154b34384ba95be3b87378
Keiran <keinn2210@gmail.com>
2026-07-27 00:06:15 +0800

    feat: reverse migration

--- supabase/migrations/0010_sparkling_jimmy_woo.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE "medication_stock" ALTER COLUMN "stock_status" SET DATA TYPE text;--> statement-breakpoint
-ALTER TABLE "medication_stock" ALTER COLUMN "stock_status" SET DEFAULT 'active'::text;--> statement-breakpoint
-DROP TYPE "public"."medication_status";--> statement-breakpoint
-CREATE TYPE "public"."medication_status" AS ENUM('active', 'discarded', 'donated', 'dispensed', 'reserved');--> statement-breakpoint
-ALTER TABLE "medication_stock" ALTER COLUMN "stock_status" SET DEFAULT 'active'::"public"."medication_status";--> statement-breakpoint
-ALTER TABLE "medication_stock" ALTER COLUMN "stock_status" SET DATA TYPE "public"."medication_status" USING "stock_status"::"public"."medication_status";--> statement-breakpoint
-ALTER TABLE "medication_active_ingredients" ADD COLUMN "remarks" text NOT NULL;--> statement-breakpoint
-ALTER TABLE "medication_brands" ADD COLUMN "remarks" text NOT NULL;--> statement-breakpoint
-ALTER TABLE "medication_stock" ADD COLUMN "remarks" text NOT NULL;
