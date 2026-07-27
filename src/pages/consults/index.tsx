b40e97ab8c1a2028a02007fef2e31c4e13d6f71f
Derrick Low <lowderrickym@gmail.com>
2026-07-27 22:06:28 +0800

    feat(consults): add sm:p-6 to the page wrapper div

--- src/pages/consults/index.tsx
+++ src/pages/consults/index.tsx
@@ -89,7 +89,7 @@ function ConsultsPage() {
   }
 
   return (
-    <div className="min-h-screen flex-1 p-4">
+    <div className="min-h-screen flex-1 p-4 sm:p-6">
       <Breadcrumbs
         items={[{ label: "Home", href: "/" }, { label: "Consults" }]}
       />
