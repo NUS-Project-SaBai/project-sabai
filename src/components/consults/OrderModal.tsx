7cae6fe9bbec092332ef628a8a1f112abbc5d486
Keiran <keinn2210@gmail.com>
2026-08-13 13:05:56 +0800

    feat: add medicine dropdown to OrderModal

--- src/components/consults/OrderModal.tsx
+++ src/components/consults/OrderModal.tsx
@@ -1,10 +1,43 @@
 import Modal from "@/components/interactive/Modal";
-import { UseFieldArrayAppend, UseFieldArrayRemove } from "react-hook-form";
+import { trpc } from "@/utils/trpc";
+import { UseFieldArrayAppend } from "react-hook-form";
+import LoadingSpinner from "@/components/LoadingSpinner";
+import { RHFDropdown } from "@/components/interactive/RHF/RHFDropdown";
 
 export default function OrderModal({
   setOrderModalIsOpen,
 }: {
   setOrderModalIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }) {
-  return <Modal onClose={() => setOrderModalIsOpen(false)}>test</Modal>;
+  const utils = trpc.useUtils();
+
+  const { data, isLoading, isError, error } =
+    trpc.medicationStockRouter.listGroupByActiveIngredient.useQuery();
+
+  return (
+    <Modal onClose={() => setOrderModalIsOpen(false)}>
+      <h2 className="text-xl font-bold mb-4">Add Order</h2>
+      Patient allergies placeholder
+      {isError && (
+        <h1 className="text-red-500">
+          An error has occurred while loading medicine.
+        </h1>
+      )}
+      {isLoading && (
+        <LoadingSpinner message="Loading medicine..." className="p-6" />
+      )}
+      {!isLoading && !isError && (
+        <RHFDropdown
+          name="medicine"
+          label="Medicine:"
+          dropdownOptions={data!.map((elem) => ({
+            value: `${elem.id}`,
+            label: `${elem.activeIngredientName}, (${elem.unitOfMeasurement}) (Qty: ${elem.quantity})`,
+          }))}
+          className="mb-4"
+          isRequired
+        />
+      )}
+    </Modal>
+  );
 }
